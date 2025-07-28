7f6895008457ada4d9300e4a5cb3abd89015a5b4
Samuel Vítor <samuelvitor668@gmail.com>
2025-07-27 22:52:23 -0300

    chore(ui): integrar ngx-toastr para feedback de login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(),  provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      ToastrModule.forRoot({
+        positionClass: 'toast-bottom-right',
+        timeOut: 3000,
+        preventDuplicates: true,
+      })
+    ),
+  ],
 };
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,13 +3,20 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { LoginService } from './login.service';
 import { Router } from '@angular/router';
 import { LoginRequestInterface } from '../interfaces/login-interface';
 import { HttpClientModule } from '@angular/common/http';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -24,8 +31,8 @@ import { CommonModule } from '@angular/common';
     ReactiveFormsModule,
     HttpClientModule,
     MatProgressSpinnerModule,
-    CommonModule
-  ]
+    CommonModule,
+  ],
 })
 export class LoginComponent implements OnInit {
   show: boolean = false;
@@ -36,37 +43,39 @@ export class LoginComponent implements OnInit {
     private fb: FormBuilder,
     private loginService: LoginService,
     private router: Router,
-  ) { }
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit() {
-
-    this.formLoginContructor()
+    this.formLoginContructor();
   }
 
   formLoginContructor() {
     this.loginForm = this.fb.group({
       email: ['', Validators.required],
-      password: ['', Validators.required]
-    })
+      password: ['', Validators.required],
+    });
   }
 
   login() {
     this.loading = true;
 
     setTimeout(() => {
-      this.loginService.login(this.loginForm.value as LoginRequestInterface).subscribe({
-        next: (response) => {
-          console.log('Login successful', response);
-          this.loading = false
-        },
-        error: (error) => {
-          console.error('Login failed', error);
-          this.loading = false;
-        }
-      })
-    }, 8000);
-
-
-
+      this.loginService
+        .login(this.loginForm.value as LoginRequestInterface)
+        .subscribe({
+          next: () => {
+            this.toastr.success(
+              'Login realizado com sucesso!',
+              'Bem-vindo de volta!'
+            );
+            this.loading = false;
+          },
+          error: () => {
+            this.toastr.error('E-mail ou senha incorretos.', 'Falha no login');
+            this.loading = false;
+          },
+        });
+    }, 1000);
   }
 }
